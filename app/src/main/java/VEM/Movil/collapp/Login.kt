28bbd615fa93ed2f12d1bef83e7432a2135ff39f
Eliu <supergonzalez360@gmail.com>
2021-05-07 16:22:44 -0700

    Pequeños ajustes

--- app/src/main/java/VEM/Movil/collapp/Login.kt
+++ app/src/main/java/VEM/Movil/collapp/Login.kt
@@ -1,11 +1,25 @@
 package VEM.Movil.collapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_login.*
 
 class Login : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+
+        tv_signup.setOnClickListener {
+            val intent = Intent(this, Register::class.java)
+            startActivity(intent)
+        }
+
+        btn_login.setOnClickListener {
+            val intent = Intent(this, Home::class.java)
+            startActivity(intent)
+        }
+
     }
 }
