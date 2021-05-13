044ec9ffef1aca292cffcce41a87668488a8cd6f
Vicgtz09 <78319920+Vicgtz@users.noreply.github.com>
2021-05-12 21:34:59 -0600

    diseño terminado

---
+++ app/src/main/java/VEM/Movil/collapp/project_detail.kt
@@ -0,0 +1,11 @@
+package VEM.Movil.collapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class project_detail : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_project_detail)
+    }
+}
