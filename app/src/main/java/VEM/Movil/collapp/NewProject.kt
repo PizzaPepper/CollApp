65a7ff62df6eda70b0f1dea89af76e081b70b9be
Misael Mendoza <misael.mendoza@potros.itson.edu.mx>
2021-05-15 18:55:20 -0700

    Diseño New Project v1.4 Misael

---
+++ app/src/main/java/VEM/Movil/collapp/NewProject.kt
@@ -0,0 +1,11 @@
+package VEM.Movil.collapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class NewProject : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_new_project)
+    }
+}
---
+++ app/src/main/java/VEM/Movil/collapp/Stages.kt
@@ -0,0 +1,11 @@
+package VEM.Movil.collapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Stages : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_stages)
+    }
+}
